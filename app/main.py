d38f27085bcc38739295937e7424c79303e2a0ce
100mi <smtrgupta96@gmail.com>
2022-01-04 18:22:00 +0530

    feat : add CORS with few origin and middleware to calculate time and represent it as header

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.routers.profile import profile_router
 from app.api.api_v1.routers.s3_events import s3_event_router
@@ -9,6 +12,29 @@ settings = Settings()
 app = FastAPI(
     title=settings.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api"
 )
+
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 @app.get(settings.API_V1_STR)

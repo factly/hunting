c9aa242f565b1bf5f99a0c46d06b8453ffb67ea8
100mi <smtrgupta96@gmail.com>
2023-09-12 13:11:09 +0530

    fix: add extra folders to dockerignore
    - update dependency for fastapi and uvicorn
    
    - change typehinting for Scatter

--- app/models/scatter.py
+++ app/models/scatter.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Union
 
 from pydantic import BaseModel
 
 
 class Scatter(BaseModel):
-    data: Dict[str, Any]
+    data: Optional[Union[Dict, Any]]
